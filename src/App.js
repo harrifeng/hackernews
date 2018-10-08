a44ea6d5f9ae8f18146f20055d32cca20fbfa8a1
Haoran Feng <harrifeng@gmail.com>
2018-10-08 06:46:39 +0000

    new ok

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var helloworld = "Weleome to the Road to learn React"
     return (
       <div className="App">
-        <h2>Weleome to the Road to learn React</h2>
+        <h2>{helloworld}</h2>
       </div>
     );
   }
