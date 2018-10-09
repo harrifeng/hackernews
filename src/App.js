6c1b8d67e93d8c726a4c376afd24854788ebfc2e
Haoran Feng <harrifeng@gmail.com>
2018-10-09 11:49:20 +0000

    new ok

--- src/App.js
+++ src/App.js
@@ -39,6 +39,14 @@ class App extends Component {
                      <span>{item.author}</span>
                      <span>{item.num_comments}</span>
                      <span>{item.points}</span>
+                     <span>
+                       <button
+                         onClick={()=> this.onDismiss(item.objectID)}
+                         type="button"
+                       >
+                         Dismiss
+                       </button>
+                     </span>
                    </div>;
           })
         }
