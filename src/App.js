c52e195ea37bc617b724680299b9c3dbb2825ff5
Haoran Feng <harrifeng@gmail.com>
2018-10-09 12:11:14 +0000

    new ok

--- src/App.js
+++ src/App.js
@@ -31,6 +31,12 @@ class App extends Component {
     this.onDismiss = this.onDismiss.bind(this);
   }
 
+  onDismiss(id) {
+    const isNotID = item => item.objecID !== id;
+    const updatedList = this.state.list.filter(isNotID);
+    this.setState({list: updatedList});
+  }
+
   render() {
     return (
       <div className="App">
@@ -43,7 +49,7 @@ class App extends Component {
                      <span>{item.points}</span>
                      <span>
                        <button
-                         onClick={()=> this.onDismiss(item.objectID)}
+                         onClick={() => this.onDismiss(item.objectID)}
                          type="button"
                        >
                          Dismiss
