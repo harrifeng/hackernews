86f57cfe6e48651db16b7a588e6c00c75a2aa07d
Haoran Feng <harrifeng@gmail.com>
2018-10-10 11:52:42 +0000

    new ok

--- src/ExplainBindingComponent.js
+++ src/ExplainBindingComponent.js
@@ -1,11 +1,11 @@
 class ExplainBindingsComponent extends Component {
-  onClickMe() {
+  onClickMe = () => {
     console.log(this)
   }
 
   render() {
     return (
-      <button onClick={this.onClickMe.bind(this)} type="button">
+      <button onClick={this.onClickMe} type="button">
         Click Me
       </button>
     );
