c919b9db0eb9e34d63059da8fd247b2fb62329e0
Haoran Feng <harrifeng@gmail.com>
2018-10-12 03:44:40 +0000

    go on

--- src/App.js
+++ src/App.js
@@ -50,6 +50,7 @@ class App extends Component {
   }
 
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
         <form>
@@ -58,7 +59,7 @@ class App extends Component {
           />
         </form>
         {
-          this.state.list.filter(isSearched(this.state.searchTerm)).map( item =>  {
+          list.filter(isSearched(searchTerm)).map( item =>  {
             return <div key={item.objectID}>
                      <span><a href={item.url}>{item.title}</a></span>
                      <span>{item.author}</span>
