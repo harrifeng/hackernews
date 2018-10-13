38ec7ec1ea51fd8b57b7acf2e22d26a0aa6e978e
Haoran Feng <harrifeng@gmail.com>
2018-10-13 03:41:58 +0000

    new ok

--- src/App.js
+++ src/App.js
@@ -45,10 +45,18 @@ const Table = ({ list, pattern, onDismiss }) => {
       {
         list.filter(isSearched(pattern)).map( item =>  {
           return <div key={item.objectID} className="table-row">
-              <span><a href={item.url}>{item.title}</a></span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
+                   <span style={{ width: '40%' }}>
+                     <a href={item.url}>{item.title}</a>
+                   </span>
+                   <span style={{ width: '30%' }}>
+                     {item.author}
+                   </span>
+                   <span style={{ width: '10%' }}>
+                     {item.num_comments}
+                   </span>
+                   <span style={{ width: '10%' }}>
+                     {item.points}
+                   </span>
               <span>
                 <Button onClick={()=> onDismiss(item.objectID)}>
                   Dismiss
